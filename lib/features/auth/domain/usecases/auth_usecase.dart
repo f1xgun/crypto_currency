64e744a71ec8650cd9596c6f58dd8cc8a7c6d090
f1xgun <msnzlmm@mail.ru>
2023-09-11 12:39:26 +0300

    feat(auth): add usecase

---
+++ lib/features/auth/domain/usecases/auth_usecase.dart
@@ -0,0 +1,46 @@
+import 'package:crypto_currency/core/services/storage/secure_storage.dart';
+import 'package:crypto_currency/features/auth/domain/entities/user.dart';
+import 'package:crypto_currency/features/auth/domain/repositories/auth_repository.dart';
+
+class AuthUseCase {
+  final AuthRepository authRepository;
+  final SecureStorage secureStorage;
+
+  const AuthUseCase({
+    required this.authRepository,
+    required this.secureStorage,
+  });
+
+  Future<User> signIn({required String email, required String password}) async {
+    try {
+      final (user, token) =
+          await authRepository.signIn(email: email, password: password);
+
+      await secureStorage.write('token', token);
+
+      return user;
+    } on Object catch (_) {
+      rethrow;
+    }
+  }
+
+  Future<bool> signUp({required String email, required String password}) async {
+    try {
+      final signUpResult =
+          await authRepository.signUp(email: email, password: password);
+
+      return signUpResult;
+    } on Object catch (_) {
+      rethrow;
+    }
+  }
+
+  Future<bool> logOut() async {
+    try {
+      await secureStorage.delete('token');
+      return true;
+    } on Object catch (_) {
+      rethrow;
+    }
+  }
+}
