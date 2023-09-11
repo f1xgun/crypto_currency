b10a9f4f8c708f150b5d74ff4c198d36f01586e6
f1xgun <msnzlmm@mail.ru>
2023-09-11 12:37:41 +0300

    feat(network): add auth interceptor to get token in headers

---
+++ lib/core/services/network/interceptors/auth_interceptor.dart
@@ -0,0 +1,26 @@
+import 'package:crypto_currency/core/services/storage/secure_storage.dart';
+import 'package:dio/dio.dart';
+
+class AuthInterceptor implements Interceptor {
+  final SecureStorage _secureStorage;
+
+  const AuthInterceptor({required SecureStorage secureStorage})
+      : _secureStorage = secureStorage;
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) {
+    // TODO: implement onError
+  }
+
+  @override
+  Future<void> onRequest(
+      RequestOptions options, RequestInterceptorHandler handler) async {
+    options.headers['Authorization'] = await _secureStorage.getToken();
+    handler.next(options);
+  }
+
+  @override
+  void onResponse(Response<dynamic> response, ResponseInterceptorHandler handler) {
+    // TODO: implement onResponse
+  }
+}
