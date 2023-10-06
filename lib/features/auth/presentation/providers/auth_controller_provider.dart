6dab0fe803881d1450c6e66a322e91254402b644
f1xgun <msnzlmm@mail.ru>
2023-10-06 20:50:35 +0300

    fix(auth): auth controller states

--- lib/features/auth/presentation/providers/auth_controller_provider.dart
+++ lib/features/auth/presentation/providers/auth_controller_provider.dart
@@ -1,9 +1,9 @@
-import 'package:crypto_currency/core/exceptions/app_state.dart';
 import 'package:crypto_currency/features/auth/domain/providers/auth_usecase_provider.dart';
 import 'package:crypto_currency/features/auth/presentation/controllers/auth_controller.dart';
+import 'package:crypto_currency/features/auth/presentation/controllers/auth_state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final authControllerProvider =
-    StateNotifierProvider.autoDispose<AuthController, AppState>((ref) {
+    StateNotifierProvider.autoDispose<AuthController, AuthState>((ref) {
   return AuthController(authUseCase: ref.read(authUseCaseProvider));
 });
